cf6c884dd13eebcde01944a5873338b13d5efab0
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-04-09 14:15:37 -0600

    lab3

---
+++ Laboratorios/Laboratorio3/laboratorio3/laboratorio3/Models/MovieModel.cs
@@ -0,0 +1,13 @@
+﻿namespace laboratorio3.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public  DateTime ReleaseDate { get; set; } 
+    }
+}
