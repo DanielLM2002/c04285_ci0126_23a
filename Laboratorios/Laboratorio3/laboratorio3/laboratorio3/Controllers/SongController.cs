c4bd58de94fc4f7e87c89bd14541a7ca0d3b9120
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-04-09 23:53:58 -0600

    Controlador y modelo de canciones

---
+++ Laboratorios/Laboratorio3/laboratorio3/laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,50 @@
+﻿using laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var songs = getListOfSongs();
+            ViewBag.MainTitle = "These are my favorite Songs of all time"; return View(songs);
+        }
+        private List<SongModel> getListOfSongs()
+        {
+            List<SongModel> songs = new List<SongModel>(); songs.Add(new SongModel
+            {
+                Id = 1,
+                SongName = "What you know",
+                SongGenre = "Indie/Rock",
+                Album = "Tourist History",
+                Artist = "Two Door Cinema Club",
+                ProducedBy = "Elliot James",
+                ReleaseDate = new DateTime(2011, 2, 7)
+            });
+            songs.Add(new SongModel
+            {
+                Id = 2,
+                SongName = "Cigarette Daydreams",
+                SongGenre = "Indie/rock",
+                Album = "Melophobia",
+                Artist = "Cage the Elephant",
+                ProducedBy = "Jay Joyce",
+                ReleaseDate = new DateTime(2014, 8, 26)
+
+            });
+            songs.Add(new SongModel
+            {
+                Id = 3,
+                SongName = "Family ties",
+                SongGenre = "Rap/Hip hop",
+                Album = "The Melodic Blue",
+                Artist = "Baby Keem/Kendrick Lamar",
+                ProducedBy = "Baby Keem",
+                ReleaseDate = new DateTime(2021, 8, 27)
+
+            });
+            return songs;
+        }
+    }
+}
---
+++ Laboratorios/Laboratorio3/laboratorio3/laboratorio3/Models/SongModel.cs
@@ -0,0 +1,19 @@
+﻿namespace laboratorio3.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+
+        public string SongName { get; set; }
+
+        public string SongGenre { get; set; }
+
+        public string Album { get; set; }
+
+        public string Artist { get; set; }
+
+        public string ProducedBy { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
