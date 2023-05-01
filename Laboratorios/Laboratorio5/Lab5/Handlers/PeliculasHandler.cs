8efff9f364fada74a034a7c875f498ececbdc72a
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-04-30 20:47:23 -0600

    commit inicial

---
+++ Laboratorios/Laboratorio5/Lab5/Handlers/PeliculasHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Lab5.Models;
+
+namespace Lab5.Handlers
+{
+    public class PeliculasHandler
+    {
+        private SqlConnection conexion;
+        private string rutaConexion;
+        public PeliculasHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion = builder.Configuration.GetConnectionString("ContextoDePeliculas");
+            conexion = new SqlConnection(rutaConexion);
+        }
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
+            SqlDataAdapter adaptadorParaTabla = new
+            SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+        public List<peliculaModelo> ObtenerPeliculas()
+        {
+            List<peliculaModelo> peliculas = new List<peliculaModelo>();
+            string consulta = "SELECT * FROM Pelicula";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                peliculas.Add(
+                new peliculaModelo
+                {
+                    ID = Convert.ToInt32(columna["Id"]),
+                    Nombre = Convert.ToString(columna["Nombre"]),
+                    Anno = Convert.ToInt32(columna["Año"]),
+                });
+            }
+            return peliculas;
+            
+        }
+    }
+}
---
+++ Laboratorios/Laboratorio5/Lab5/Models/peliculaModelo.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab5.Models
+{
+    public class peliculaModelo
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Anno { get; set; }
+    }
+}
