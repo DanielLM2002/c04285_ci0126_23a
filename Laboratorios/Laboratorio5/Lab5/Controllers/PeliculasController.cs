3c1d2b7daa2ed830b7d4c64c16f561ab8b727fd6
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-04-30 20:52:02 -0600

    controlador de peliculas

---
+++ Laboratorios/Laboratorio5/Lab5/Controllers/PeliculasController.cs
@@ -0,0 +1,16 @@
+﻿using Lab5.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        public IActionResult Index()
+        {
+            PeliculasHandler peliculasHandler = new PeliculasHandler();
+            var peliculas = peliculasHandler.ObtenerPeliculas();
+            ViewBag.MainTitle = "Lista de peliculas";
+            return View(peliculas);
+        }
+    }
+}
