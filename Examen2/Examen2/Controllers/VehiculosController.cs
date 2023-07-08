41b45b86bbec093d5a18b8163be1f2e71ffdacf3
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-07-07 22:57:39 -0600

    Se crea Plantilla MVC para examen

---
+++ Examen2/Examen2/Controllers/VehiculosController.cs
@@ -0,0 +1,42 @@
+﻿using Examen2.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace laboratorio3.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vehiculos = GetListOfVehiculos();
+            ViewBag.MainTitle = "These are my favorite movies"; return View(vehiculos);
+        }
+        private List<VehiculosModel> GetListOfVehiculos()
+        {
+            List<VehiculosModel> vehiculos = new List<VehiculosModel>(); vehiculos.Add(new VehiculosModel
+            {
+                Nombre = "Toyota Camry",
+                Tipo = "Terrestre",
+                Popularidad = "Alta",
+                Precio = 50000,
+                NecesitaLicencia = true
+            });
+            vehiculos.Add(new VehiculosModel
+            {
+                Nombre = "Toyota Corolla",
+                Tipo = "Terrestre",
+                Popularidad = "Alta",
+                Precio = 40000,
+                NecesitaLicencia = true
+            });
+            vehiculos.Add(new VehiculosModel
+            {
+                Nombre = "Bote simple",
+                Tipo = "Acuatico",
+                Popularidad = "baja",
+                Precio = 10000,
+                NecesitaLicencia = false
+            });
+            return vehiculos;
+
+        }
+    }
+}
---
+++ Examen2/Examen2/Models/VehiculosModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Examen2.Models
+{
+    public class VehiculosModel
+    {
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public string Popularidad { get; set; }
+
+        public int Precio { get; set; }
+
+        public bool NecesitaLicencia { get; set; }
+    }
+}
