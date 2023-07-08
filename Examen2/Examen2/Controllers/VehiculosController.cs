57a8c3cfb490b96c25567398abb14a4c21f2d282
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-07-08 12:35:18 -0600

    Cambios a connection string

--- Examen2/Examen2/Controllers/VehiculosController.cs
+++ Examen2/Examen2/Controllers/VehiculosController.cs
@@ -1,42 +1,16 @@
 ﻿using Examen2.Models;
+using Examen2.Handlers;
 using Microsoft.AspNetCore.Mvc;
-namespace laboratorio3.Controllers
+namespace Examen2.Controllers
 {
     public class VehiculosController : Controller
     {
         public IActionResult Index()
         {
-            var vehiculos = GetListOfVehiculos();
-            ViewBag.MainTitle = "These are my favorite movies"; return View(vehiculos);
-        }
-        private List<VehiculosModel> GetListOfVehiculos()
-        {
-            List<VehiculosModel> vehiculos = new List<VehiculosModel>(); vehiculos.Add(new VehiculosModel
-            {
-                Nombre = "Toyota Camry",
-                Tipo = "Terrestre",
-                Popularidad = "Alta",
-                Precio = 50000,
-                NecesitaLicencia = true
-            });
-            vehiculos.Add(new VehiculosModel
-            {
-                Nombre = "Toyota Corolla",
-                Tipo = "Terrestre",
-                Popularidad = "Alta",
-                Precio = 40000,
-                NecesitaLicencia = true
-            });
-            vehiculos.Add(new VehiculosModel
-            {
-                Nombre = "Bote simple",
-                Tipo = "Acuatico",
-                Popularidad = "baja",
-                Precio = 10000,
-                NecesitaLicencia = false
-            });
-            return vehiculos;
-
+            VehiculosHandler vehiculosHandler = new VehiculosHandler(); 
+            var vehiculos = vehiculosHandler.ObtenerVehiculos();
+            ViewBag.MainTittle = "Lista de vehiculos";
+            return View(vehiculos);
         }
     }
 }
---
+++ Examen2/Examen2/Handlers/VehiculosHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Examen2.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Examen2.Handlers
+{
+    public class VehiculosHandler
+    {
+        private SqlConnection conexion;
+        private string rutaConexion;
+        public VehiculosHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion = builder.Configuration.GetConnectionString("ContextoVehiculos");
+            conexion = new SqlConnection(rutaConexion);
+        }
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta, conexion);
+            SqlDataAdapter adaptadorParaTabla = new SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+        public List<VehiculosModel> ObtenerVehiculos()
+        {
+            List<VehiculosModel> vehiculos = new List<VehiculosModel>();
+            string consulta = "SELECT * FROM Vehiculo";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                vehiculos.Add(
+                new VehiculosModel
+                {
+                    //ID = Convert.ToInt32(columna["Id"]),
+                    //Nombre = Convert.ToString(columna["Nombre"]),
+                    //Año = Convert.ToInt32(columna["Año"]),
+                    Nombre = Convert.ToString(columna["Nombre"]),
+                    Tipo = Convert.ToString(columna["Tipo"]),
+                    Popularidad = Convert.ToString(columna["Popularidad"]),
+                    Precio = Convert.ToInt32(columna["Precio"]),
+                    NecesitaLicencia = Convert.ToBoolean(columna["NecesitaLicencia"]),
+
+                });
+            }
+            return vehiculos;
+        }
+    }
+}
