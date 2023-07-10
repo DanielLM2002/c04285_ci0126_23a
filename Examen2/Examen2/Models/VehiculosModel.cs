49488b9f20a6c342681f17781008770caeefb138
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-07-10 02:24:02 -0600

    Nuevas pruebas

--- Examen2/Examen2/Models/VehiculosModel.cs
+++ Examen2/Examen2/Models/VehiculosModel.cs
@@ -7,14 +7,17 @@ namespace Examen2.Models
     {
         [Required(ErrorMessage = "Debe de ingresar un nombre")]
         [DisplayName("Nombre del vehiculo")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "No puede ingresar numeros")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "Debe de ingresar el tipo de vehiculo (terrestre, acuatico u anfibio)")]
         [DisplayName("Tipo de vehiculo")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "No puede ingresar numeros")]
         public string Tipo { get; set; }
 
         [Required(ErrorMessage = "Debe de ingresar la popularidad del vehiculo (baja, media o alta)")]
         [DisplayName("Popularidad de vehiculo")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "No puede ingresar numeros")]
         public string Popularidad { get; set; }
 
         [Required(ErrorMessage = "Ingrese el precio en dolares")]
@@ -28,7 +31,7 @@ namespace Examen2.Models
 
 
         [Required(ErrorMessage = "Debe de ingresar un ID valido")]
-        [DisplayName("Id del vehiculo")]
+        [DisplayName("Id del vehiculo, ingrese un numero entero")]
         [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Ingrese un ID valido")]
         public int ID { get; set; }
     }
---
+++ Examen2/TestProject1/UnitTestModel.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Examen2.Models;
+
+namespace Examen2.Tests
+{
+    [TestClass]
+    public class VehiculosModelTests
+    {
+        [TestMethod]
+        public void TestNombreValidation()
+        {
+            VehiculosModel vehiculo = new VehiculosModel();
+            vehiculo.Nombre = "";
+            var context = new System.ComponentModel.DataAnnotations.ValidationContext(vehiculo, null, null);
+            var results = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool isValid = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(vehiculo, context, results);
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void TestTipoValidation()
+        {
+            VehiculosModel vehiculo = new VehiculosModel();
+            vehiculo.Tipo = "";
+            var context = new System.ComponentModel.DataAnnotations.ValidationContext(vehiculo, null, null);
+            var results = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool isValid = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(vehiculo, context, results);
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void TestPopularidadValidation()
+        {
+            VehiculosModel vehiculo = new VehiculosModel();
+            vehiculo.Popularidad = "";
+            var context = new System.ComponentModel.DataAnnotations.ValidationContext(vehiculo, null, null);
+            var results = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool isValid = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(vehiculo, context, results);
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void TestPrecioValidation()
+        {
+            VehiculosModel vehiculo = new VehiculosModel();
+            vehiculo.Precio = -1;
+            var context = new System.ComponentModel.DataAnnotations.ValidationContext(vehiculo, null, null);
+            var results = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool isValid = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(vehiculo, context, results);
+            Assert.IsFalse(isValid);
+        }
+    }
+}
