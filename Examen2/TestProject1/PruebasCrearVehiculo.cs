35cc1a229210bf1e45e108d5ddda2e37d745baef
Daniel Lizano <dlizano@outlook.com>
2023-07-11 01:28:26 -0600

    Documentacion de pruebas

--- Examen2/TestProject1/PruebasCrearVehiculo.cs
+++ Examen2/TestProject1/PruebasCrearVehiculo.cs
@@ -10,6 +10,16 @@ namespace Examen2Tests
     [TestClass]
     public class VehiculosControllerTests
     {
+        /// <summary>
+        /// Esta prueba busca verificar si el método Index devuelve un objeto ViewResult
+        /// y si el modelo de ese objeto cuenta con una lista del tipo VehiculosModel.
+        /// Al principio en el Arrange la prueba crea una instancia de la clase VehiculosController
+        /// para poder llamar al método Index.
+        /// Mas adelante en el Act se llama el método Index y el resultado es almacenado en
+        /// la variable resultado.
+        /// Por ultimo en los assert se realizan las verificaciones para ver si
+        /// lo que se encuentra en result es una instancia de ViewResult
+        /// </summary>
         [TestMethod]
         public void Index_ReturnsViewResult_WithListOfVehiculos()
         {
@@ -25,6 +35,16 @@ namespace Examen2Tests
             Assert.IsInstanceOfType(viewResult.Model, typeof(List<VehiculosModel>));
         }
 
+        /// <summary>
+        /// Este test probar que el método CrearVehiculos retorne un ViewResult
+        /// Para ello en el Arrange se crea una instancia de la clase "VehiculosController" y
+        /// se agrega un error con el método "AddModelError" del objeto "ModelState".
+        /// Esto simula un estado de modelo inválido donde se requiere el campo "Nombre" pero no se proporciona.
+        /// Mas adelante en el Act se llama a CrearVehiculo con el fin de pasar un nuevo
+        /// objeto llamado VehiculosModel como parámetro. Este estará vacío y lo almacena en la variable result.
+        /// En el Assert se revisa si es una instancia de ViewModel y si el resultado es igual al ViewData.
+        /// Si ambos son positivos la prueba es exitosa.
+        /// </summary>
         [TestMethod]
         public void CrearVehiculo_ReturnsViewResult_WhenModelStateIsInvalid()
         {
