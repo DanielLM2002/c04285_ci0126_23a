45b2f6e7476d353295363879fd9136137409a9ac
Daniel Lizano <dlizano@outlook.com>
2023-07-11 11:57:43 -0600

    primera prueba selenium

---
+++ Examen2/SeleniumCore/FuncionalTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class HomePageFeature
+    {
+        [TestMethod]
+        public void ReadData()
+        {
+            var outputDirectory = Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location);
+            var driver = new ChromeDriver(outputDirectory);
+            driver.Navigate().GoToUrl("https://saucedemo.com");
+
+        }
+    }
+}
+
