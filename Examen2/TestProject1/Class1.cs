8f808e8a2471d4470e7b775581bd69d1d0520d45
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-07-10 18:34:31 -0600

    nuevas pruebas

--- Examen2/TestProject1/Class1.cs
+++ Examen2/TestProject1/Class1.cs
@@ -1,38 +1,48 @@
-﻿using Examen2.Controllers;
+﻿
+using System;
+using Examen2.Controllers;
 using Examen2.Models;
 using Examen2.Handlers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using System.Collections.Generic;
 
 namespace Examen2.Tests
 {
     [TestClass]
     public class VehiculosControllerTests
     {
-        private VehiculosController _vehiculosController;
-        private Mock<VehiculosHandler> _mockVehiculosHandler;
-
-        [TestInitialize]
-        public void TestInitialize()
+        [TestMethod]
+        public void EliminarVehiculo_IdNull_RedirectsToIndex()
         {
-            _mockVehiculosHandler = new Mock<VehiculosHandler>();
-            _vehiculosController = new VehiculosController();
+            // Arrange
+            var controller = new VehiculosController();
+            int? id = null;
+
+            // Act
+            var result = controller.EliminarVehiculo(id) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual(null, result.ControllerName);
         }
 
-
-
         [TestMethod]
-        public void CrearVehiculo_ReturnsAViewResult_WithAVehiculosModel()
+        public void EliminarVehiculo_VehiculoExists_RedirectsToIndex()
         {
+            // Arrange
+            var controller = new VehiculosController();
+            var vehiculosHandler = new VehiculosHandler();
+            var vehiculo = new VehiculosModel() { ID = 1, Nombre = "Test vehiculo" };
+            var created = vehiculosHandler.CrearVehiculo(vehiculo);
+
             // Act
-            var result = _vehiculosController.CrearVehiculo();
+            var result = controller.EliminarVehiculo(vehiculo.ID) as RedirectToActionResult;
+
             // Assert
-            Assert.IsInstanceOfType(result, typeof(ViewResult));
-            var viewResult = (ViewResult)result;
-            Assert.IsInstanceOfType(viewResult.Model, typeof(VehiculosModel));
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual("Vehiculos", result.ControllerName);
         }
     }
 }
-
