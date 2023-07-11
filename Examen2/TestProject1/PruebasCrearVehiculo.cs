195c9af2e1f7fdb8729ddcc4b4ba6c698d09bdbe
Daniel Lizano <63821814+DanielLM2002@users.noreply.github.com>
2023-07-10 19:32:04 -0600

    ultimo batch de pruebas unitarias

---
+++ Examen2/TestProject1/PruebasCrearVehiculo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Examen2.Controllers;
+using Examen2.Models;
+using Examen2.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Examen2Tests
+{
+    [TestClass]
+    public class VehiculosControllerTests
+    {
+        [TestMethod]
+        public void Index_ReturnsViewResult_WithListOfVehiculos()
+        {
+            // Arrange
+            var vehiculosController = new VehiculosController();
+
+            // Act
+            var result = vehiculosController.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.Model, typeof(List<VehiculosModel>));
+        }
+
+        [TestMethod]
+        public void CrearVehiculo_ReturnsViewResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var vehiculosController = new VehiculosController();
+            vehiculosController.ModelState.AddModelError("Nombre", "El nombre es requerido");
+
+            // Act
+            var result = vehiculosController.CrearVehiculo(new VehiculosModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual(false, viewResult.ViewData["ExitoAlCrear"]);
+        }
+    }
+}
+
